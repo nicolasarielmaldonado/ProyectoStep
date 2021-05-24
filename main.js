fe26c8c7aefb8189394082d0d3dfec690edfedd2
nicolas ariel maldonado <nicolas.ariel.maldonado@gmail.com>
2021-05-23 21:31:29 -0300

    logica colisiones + limpieza de codigo

--- main.js
+++ main.js
@@ -1,86 +1,42 @@
-//Typescript
 'use strict';
+
 
 //Protagonista y sus caracteristicas
 const jugador = {
     sprite: '/src/prota.png',
     posicion: {
-        posicionX : 8,
-        posicionY : 9,
+        posicionX : 6,
+        posicionY : 10,
     },
-    pasos: 100
+    maxPasos: 100,
+    pasos: 100,
+    maxVida: 50,
+    vida: 50,
+    inventorio: []
 }
 
-//Codigo objeto colisionable
-
-// const piedras = {
-//     piedra1:{
-//         posicionX:4,
-//         posicionY:4
-//     },
-//     piedra2:{
-//         posicionX:6,
-//         posicionY:6
-//     }
-// }
 
 // Codigo numero al azar
-
 // const NumeroAlAzar = () =>{
 //     let numerito = Math.floor(Math.random() * 2);
 //     return numerito;
 // }
 
 
-const piedras = [{x:8,y:8},{x:6,y:6}];
 
-let piedradibu;
+const piedras = [{x:2,y:3},{x:5,y:9},{x:8,y:2}];   //GENERADO RANDOM // ARRAY DE PIEDRAS
 
-const dibujarLasPiedras = () =>{
+const dibujarPiedras = () =>{
     piedras.forEach(e => {
-        piedradibu = document.getElementById(`${e.y},${e.x}`);
-        console.log(piedradibu)
+        let $piedraDOM = document.getElementById(`${e.y},${e.x}`);
         const $imagepiedra = document.createElement('img');
         $imagepiedra.src = "/src/roca.png"; 
-        piedradibu.appendChild($imagepiedra);   
+        $piedraDOM.appendChild($imagepiedra);   
     });
 }
 
-dibujarLasPiedras();
 
 
-//Dibujar piedra colicionable
-
-
-// const dibujarpiedra = () =>{
-//     piedradibu = document.getElementById(`${piedra.posicion.posicionY},${piedra.posicion.posicionX}`); //Carga la piedra en el elemento del Dom
-//     const $imagepiedra = document.createElement('img');
-//     $imagepiedra.src = piedra.sprite;  
-//     piedradibu.appendChild($imagepiedra);  
-// }
-
-// dibujarpiedra();
-
-//crear generador enemigos
-/* class Enemy {
-    constructor(AGREGAR POSICION COMO OBJETO) {
-        this.type = type;
-        this.position = {
-            positionx: 0,
-            positionY: 0
-        }
-        this.position.posX = posX;
-        this.position.posY = posY;
-    }
-}
-
-const zombie1 = new Enemy("A", {posX: 1, posY : 1});
- */
-
-//--automatizar creacion de enemigos (!)
-
-
-let $posicionActual;
 
 //Escucha del teclado y control de movimientos
 document.addEventListener("keydown", (event) => {
@@ -92,65 +48,70 @@ document.addEventListener("keydown", (event) => {
     //Ejecucion solo al presionar teclas correctas
     if (event.keyCode === 37 || event.keyCode === 38 || event.keyCode === 39 || event.keyCode === 40){
 
-        // piedras.forEach(e =>{
-        //     if( (jugador.posicion.posicionY -1) === e.y && (jugador.posicion.posicionX)=== e.x) 
-        // });
-
-        let pepo = false;
-
-        if (event.keyCode === 38){
+        if (event.keyCode === 38){//ARRIBA BUCLE QUE VERIFICA COLISION CON PIEDRAS
+            let colisionEstado = false;
             piedras.forEach(e =>{
-                if( (jugador.posicion.posicionY -1) === e.y && (jugador.posicion.posicionX)=== e.x){
-                    pepo = true;
+                if ((jugador.posicion.posicionY - 1) === e.y && (jugador.posicion.posicionX === e.x)){
+                    colisionEstado = true;
                 }
             })
+            if (!colisionEstado){
+                jugador.posicion.posicionY--;
+            }
+        }
 
-                if(pepo != false){
-                    jugador.posicion.posicionY--;
-                    pepo= false;
+        if (event.keyCode === 37){//IZQUIERDA
+            let colisionEstado = false;
+            piedras.forEach(e =>{
+                if ((jugador.posicion.posicionX - 1) === e.x && (jugador.posicion.posicionY === e.y)){
+                    colisionEstado = true;
                 }
-        
-    }
-        // if (event.keyCode === 37){
-        //     if((jugador.posicion.posicionX - 1) === piedra.posicion.posicionX && jugador.posicion.posicionY === piedra.posicion.posicionY){
-        //     }else{ //Izquierda
-        //         jugador.posicion.posicionX--;
-        //     }
-        // }
-        // if (event.keyCode === 40){
-        //     if((jugador.posicion.posicionY + 1) === piedra.posicion.posicionY && jugador.posicion.posicionX === piedra.posicion.posicionX){
-        //     }else{ //Abajo
-        //         jugador.posicion.posicionY++;
-        //     }
-        // }
-        // if (event.keyCode === 39){  
-        //     if((jugador.posicion.posicionX + 1) === piedra.posicion.posicionX && jugador.posicion.posicionY === piedra.posicion.posicionY){
-        //     }else{ //Derecha
-        //         jugador.posicion.posicionX++;
-        //     }
-        // }
+            })
+            if (!colisionEstado){
+                jugador.posicion.posicionX--;
+            }
+        }
 
-        //con la piedra
+        if (event.keyCode === 40){//ABAJO
+            let colisionEstado = false;
+            piedras.forEach(e =>{
+                if ((jugador.posicion.posicionY + 1) === e.y && (jugador.posicion.posicionX === e.x)){
+                    colisionEstado = true;
+                }
+            })
+            if (!colisionEstado){
+                jugador.posicion.posicionY++;
+            }
+        }
 
-        // if(jugador.posicion.posicionX === piedra.posicion.posicionX && jugador.posicion.posicionY === piedra.posicion.posicionY){
-        //     jugador.posicion.posicionX++;
-        // }
+        if (event.keyCode === 39){//DERECHA
+            let colisionEstado = false;
+            piedras.forEach(e =>{
+                if ((jugador.posicion.posicionX + 1) === e.x && (jugador.posicion.posicionY === e.y)){
+                    colisionEstado = true;
+                }
+            })
+            if (!colisionEstado){
+                jugador.posicion.posicionX++;
+            }
+        }
+
 
         //Funciones a realizar al moverse
-        LimpiarJugador(posicionYAnterior,posicionXAnterior);
-        DibujarMapa();
+        limpiarJugador(posicionYAnterior,posicionXAnterior);
+        dibujarMapa();
     }
 });
 
 //Borrar la posicion anterior
-const LimpiarJugador = (movimientoAnteriorY, movimientoAnteriorX) => {
+const limpiarJugador = (movimientoAnteriorY, movimientoAnteriorX) => {
     let $posicionAnterior = document.getElementById(`${movimientoAnteriorY},${movimientoAnteriorX}`);
     $posicionAnterior.innerHTML = "";
 }
 
 //Dibujar jugador en el map      //   ----->CAMBIAR A DRAW GLOBAL
-const DibujarMapa = () => { 
-    $posicionActual = document.getElementById(`${jugador.posicion.posicionY},${jugador.posicion.posicionX}`); 
+const dibujarMapa = () => { 
+    let $posicionActual = document.getElementById(`${jugador.posicion.posicionY},${jugador.posicion.posicionX}`); 
     const $imagenJugador = document.createElement('img');
     $imagenJugador.src = jugador.sprite;   //Carga en el elemento image el sprite del jugador
     $imagenJugador.id = "jugador";     //asigna id "jugador" para uso en el movimiento
@@ -161,7 +122,9 @@ const DibujarMapa = () => {
 
 //Inicar dibujado al cargar la aplicacion
 const inicializacion = () => {
-    DibujarMapa();
+    dibujarMapa();
+    dibujarPiedras();
 }
+
 
 inicializacion();
